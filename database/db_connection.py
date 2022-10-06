6c53f88626e3a15b89a3cb606b437ec5fbe6d876
Odhran <mahonyodhran@gmail.com>
2022-10-06 23:05:33 +0100

    added session/base

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,9 +1,14 @@
 """Module to connect to database
 """
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from env_vars import USER, PWD, DB, HOST, PORT
 
 
 my_sql_conn = create_engine(
     "mysql://" + USER + ":" + PWD + "@" + HOST + ":" + str(PORT) + "/" + DB
 )
+
+Session = sessionmaker(bind=my_sql_conn)
+Base = declarative_base()
