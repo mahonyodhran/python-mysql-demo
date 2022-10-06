3f3125bfaf72cabf8db32e78c11c40c297ed4d7c
Odhran <mahonyodhran@gmail.com>
2022-10-06 20:19:43 +0100

    added docstring, black reformat

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,4 +1,9 @@
-from database.env_vars import USER, PWD, DB, HOST, PORT
+"""Module to connect to database
+"""
 from sqlalchemy import create_engine
+from env_vars import USER, PWD, DB, HOST, PORT
 
-my_sql_conn = create_engine("mysql://"+USER+":"+PWD+"@"+ HOST+":"+str(PORT)+"/"+DB)
+
+my_sql_conn = create_engine(
+    "mysql://" + USER + ":" + PWD + "@" + HOST + ":" + str(PORT) + "/" + DB
+)
