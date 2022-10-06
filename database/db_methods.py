a06dc6df06067731e6f5a775ebf5c472a6369891
Odhran <mahonyodhran@gmail.com>
2022-10-06 20:20:04 +0100

    black reformat

--- database/db_methods.py
+++ database/db_methods.py
@@ -1,11 +1,15 @@
 from database.db_connection import my_sql_conn
 
+
 def insertQuery(first_name, last_name, email):
     values = [first_name, last_name, email]
-    my_sql_conn.execute("insert into user (first_name,last_name, email) values(%s,%s, %s)", values)
+    my_sql_conn.execute(
+        "insert into user (first_name,last_name, email) values(%s,%s, %s)", values
+    )
+
 
 def selectQuery():
-    query = my_sql_conn.execute('SELECT * FROM user;')
+    query = my_sql_conn.execute("SELECT * FROM user;")
     for row in query:
         print(row)
     query.close()
