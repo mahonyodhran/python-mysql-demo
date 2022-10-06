4969dced2f11969a4254f1c526ec5b8e3e407342
Odhran <mahonyodhran@gmail.com>
2022-10-06 23:04:12 +0100

    updated for sqlalchemy ORM style mapping

--- models/User.py
+++ models/User.py
@@ -1,8 +1,20 @@
-'''Module for User class
-'''
+from sqlalchemy import Column, String, Integer
 
-class User:
+from database.db_connection import Base
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+
     def __init__(self, first_name, last_name, email):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        
+    def __repr__(self):
+        return f"User(id={self.id!r}, first_name={self.first_name!r}, last_name={self.last_name!r},email_address={self.email!r})"
