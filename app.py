8668c7209bdd221534e27277882d4e781d67862b
Odhran <mahonyodhran@gmail.com>
2022-10-06 20:23:52 +0100

    updated method names from db.methods

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 '''Main driver for app
 '''
-from database.db_methods import selectQuery, insertQuery
+from database.db_methods import select_query, insert_query
 
 
 if __name__ == "__main__":
-    insertQuery("John", "Doe", "john@mail.com")
-    selectQuery()
+    insert_query("Mike", "Hawk", "mike@mail.com")
+    select_query()
