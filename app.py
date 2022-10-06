0dc634ee2ad2abd2ee70f6016c5ef5b8a9877b37
Odhran <mahonyodhran@gmail.com>
2022-10-06 20:09:31 +0100

    black format

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from database.db_methods import selectQuery, insertQuery
 
 
 if __name__ == "__main__":
-    insertQuery('John', 'Doe', 'john@mail.com')
+    insertQuery("John", "Doe", "john@mail.com")
     selectQuery()
