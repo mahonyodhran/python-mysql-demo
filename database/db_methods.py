3948a5ee6cd444d6d7ca03e7b7dc8b9569ba6dd5
Odhran <mahonyodhran@gmail.com>
2022-10-06 20:23:32 +0100

    added docstring - updated method names to pythonic format

--- database/db_methods.py
+++ database/db_methods.py
@@ -1,14 +1,20 @@
+'''Module for any database related methods
+'''
 from database.db_connection import my_sql_conn
 
 
-def insertQuery(first_name, last_name, email):
+def insert_query(first_name, last_name, email):
+    '''insert record into table
+    '''
     values = [first_name, last_name, email]
     my_sql_conn.execute(
         "insert into user (first_name,last_name, email) values(%s,%s, %s)", values
     )
 
 
-def selectQuery():
+def select_query():
+    '''retrieve records from table
+    '''
     query = my_sql_conn.execute("SELECT * FROM user;")
     for row in query:
         print(row)
