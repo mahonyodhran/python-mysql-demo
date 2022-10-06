23d440c49ac64a0385a7d356927e5532dde1dc21
Odhran <mahonyodhran@gmail.com>
2022-10-06 23:04:49 +0100

    updated method names,params etc for SQLAlchemy ORM work

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 '''Main driver for app
 '''
-from database.db_methods import select_query, insert_query
+from database.db_methods import select_all_users, insert_user
+from models.user import User
 
 
 if __name__ == "__main__":
-    insert_query("Mike", "Hawk", "mike@mail.com")
-    select_query()
+    newUser = User("Tom", "Hanks", "tanks@mail.com")
+    insert_user(newUser)
+    select_all_users()
