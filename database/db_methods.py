60dfe49ad4f397d03a9e4eecd231a59f4c3f16ad
Odhran <mahonyodhran@gmail.com>
2022-10-06 20:06:54 +0100

    added a module for any database methods for now

---
+++ database/db_methods.py
@@ -0,0 +1,11 @@
+from database.db_connection import my_sql_conn
+
+def insertQuery(first_name, last_name, email):
+    values = [first_name, last_name, email]
+    my_sql_conn.execute("insert into user (first_name,last_name, email) values(%s,%s, %s)", values)
+
+def selectQuery():
+    query = my_sql_conn.execute('SELECT * FROM user;')
+    for row in query:
+        print(row)
+    query.close()
